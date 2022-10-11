c521fe52acc1a2f9697804789d2d09e66829a656
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-10-11 13:00:21 +0530

    feat : simple counter

---
+++ fundamentals/loops.js
@@ -0,0 +1,18 @@
+
+
+// for each
+
+let my_array = ['alpha', 'delta', 'omega'];
+my_array.forEach((item, index, array)=>{
+    console.log(item)
+    console.log(index)
+    console.log(array)
+})
+
+
+//for in -> mainly in object case
+let obj = {
+    name: "ravi kumar",
+    age: 23,
+    sample:true
+}
