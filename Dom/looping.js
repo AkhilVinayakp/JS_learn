5ceed199dbb0aa167ddb4742ac69081177b046d7
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-10-17 11:39:57 +0530

    feat: choose random background colors

--- Dom/looping.js
+++ Dom/looping.js
@@ -1,6 +1,37 @@
+let run_configs = {
+    light_speed: 200,
+}
+
 
 let keykaps = document.querySelectorAll('.key');
 pre = "white"
+let bg_ch_button = document.querySelector('#bgchanger');
+
+bg_ch_button.addEventListener('click', ()=>{
+    rand_rgbs = gen_random_rgb();
+    document.body.style.backgroundColor = rand_rgbs.color;
+    bg_ch_button.style.backgroundColor = rand_rgbs.invColor;
+    bg_ch_button.style.color = rand_rgbs.color;
+})
+
+const random = (number)=>{
+    return Math.floor(Math.random()*(number+1))
+}
+
+const gen_random_rgb = ()=>{
+    c1 = random(255);
+    c1_inv = 255 - c1;
+    c2 = random(255);
+    c2_inv = 255 - c2;
+    c3 = random(255);
+    c3_inv = 255 - c3;
+    return {
+        'color': `rgb(${c1}, ${c2}, ${c3})`,
+        'invColor': `rgb(${c1_inv}, ${c2_inv}, ${c3_inv})`
+    }
+}
+
+
 setInterval(()=>{            
     for(let key_ of keykaps){
         current_class = key_.className;
@@ -9,4 +40,4 @@ setInterval(()=>{
         key_.classList.add(pre);
         pre=current_color;
 }
-},200)
+}, run_configs.light_speed);
