853c0d2a682bacc3f0eebdbafd2d517e43a21291
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-12-18 23:58:33 +0530

    learn :promise chaining

--- fundamentals/promise.js
+++ fundamentals/promise.js
@@ -31,26 +31,60 @@ async function Test(){
  * @description: The following code to show the failed Promise case
  ************************************/
 
-const compute = new Promise((resolve,reject)=>{
-    let t1 = "RRR"
-    let t2 = "RRR2"
-    if(t1===t2){
-        resolve("Match found")
-    }
-    else{
-        reject("Match not found");
-    }
-});
-console.log(compute)
-compute.finally(()=>{
-        console.log("Ha ha promise")
-    }).then((val)=>{
-            console.log("What we got inside the promise", val)
-        }).catch((err)=>{
-            console.log("Can not complete the promise. Promise rejected");
-    });
+// const compute = new Promise((resolve,reject)=>{
+//     let t1 = "RRR"
+//     let t2 = "RRR2"
+//     if(t1===t2){
+//         resolve("Match found")
+//     }
+//     else{
+//         reject("Match not found");
+//     }
+// });
+// console.log(compute)
+// compute.finally(()=>{
+//         console.log("Ha ha promise")
+//     }).then((val)=>{
+//             console.log("What we got inside the promise", val)
+//         }).catch((err)=>{
+//             console.log("Can not complete the promise. Promise rejected");
+//     });
 /************************************  
  * @Note: For above code
  * Finally will always run
  * when reject() executed or finally throws an error the catch block will handle the result. The promise will be rejected.
+ ************************************/
+
+
+/************************************  
+ * @description: Promise chaining
+ ************************************/
+
+let mypromise = new Promise((resolve, reject)=>{
+    let m = 21;
+    if(m == 20){
+        resolve(m)
+    }
+    else reject(new Error("500"));
+})
+
+// mypromise.then((val)=>{
+//     console.log("promise fullfiled with value:", val)
+// }, (err)=>{
+//     console.log("Promise failed with error", err)
+// })
+
+mypromise.then((val)=>{
+    console.log(val)
+    return val;// if not retured any value there would be no value passed to the chained then handler.
+}).then((secVal)=>
+    console.log("second handler value: ", secVal)
+
+).catch((err)=>{console.log(err)});
+
+
+
+
+/************************************  
+ * @description: 
  ************************************/
